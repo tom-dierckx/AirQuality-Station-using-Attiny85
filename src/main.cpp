3d0645165a176280d778273556637e7e79c219c7
Tom Dierckx <dierckx.t@telenet.be>
2020-03-24 21:53:49 +0100

    playing around with the cc811

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 // display class
 #include <LiquidCrystal_I2C.h>
 // CCS811 class
-#include <SparkFunCCS811.h>
+#include "ccs811.h"
 // SD for writing data to the SD card
 #include <SD.h>
 
@@ -18,6 +18,7 @@ const unsigned long sensorHeatupTime = 10000;   // time for sensors to heat up
 
 const unsigned int buttinpin = 7;   // pin used for button to cycle in the menu
 const unsigned int mq135pin = A0;   // connection pin for MQ135
+const unsigned int ccs811wakepin = 8;
 
 // i2c addresses
 const uint8_t lcdAddr = 0x38;
@@ -29,7 +30,7 @@ unsigned long previousbootinMillis = 0;
 unsigned int bootingdots = 0;
 
 LiquidCrystal_I2C lcd(lcdAddr, 16, 2);
-CCS811 myCCS811(ccs811Addr);
+CCS811 myCCS811(ccs811wakepin, ccs811Addr);
 
 File myFile;
 
@@ -47,8 +48,9 @@ void setup() {
   Serial.println("Started LCD");
   // setup sensors
   // setup CCS811 => carbon dioxide (eCO2) and metal oxide (MOX)
-  myCCS811.begin();
-  Serial.println("Started myCCS811");
+  if(!myCCS811.begin()){
+    Serial.println("setup: CCS811 begin FAILED");
+  }
   // setup SD card writer
   // Note that even if it's not used as the CS pin, the hardware SS pin 
   // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
@@ -92,19 +94,23 @@ void sensorLogic() {
   unsigned long currentMillis = millis();
   if(currentMillis - previousPollingMillis > sensorPollingInterval) {
     previousPollingMillis = currentMillis; 
-    if (myCCS811.dataAvailable())
-    {
-      myCCS811.readAlgorithmResults();
-      int tempCO2 = myCCS811.getCO2();
-      int tempVOC = myCCS811.getTVOC();
-      Serial.print("CO2:");
-      Serial.println(tempCO2);
-      Serial.print("Total Volatile Organic Compounds");
-      Serial.println(tempVOC);
-    }
-    else if (myCCS811.checkForStatusError())
-    {
-      Serial.print("Status error");
+    uint16_t eco2, etvoc, errstat, raw;
+    myCCS811.read(&eco2,&etvoc,&errstat,&raw); 
+    if( errstat==CCS811_ERRSTAT_OK ) { 
+      Serial.print("CCS811: ");
+      Serial.print("eco2=");  Serial.print(eco2);     Serial.print(" ppm  ");
+      Serial.print("etvoc="); Serial.print(etvoc);    Serial.print(" ppb  ");
+      //Serial.print("raw6=");  Serial.print(raw/1024); Serial.print(" uA  "); 
+      //Serial.print("raw10="); Serial.print(raw%1024); Serial.print(" ADC  ");
+      //Serial.print("R="); Serial.print((1650*1000L/1023)*(raw%1024)/(raw/1024)); Serial.print(" ohm");
+      Serial.println();
+    } else if( errstat==CCS811_ERRSTAT_OK_NODATA ) {
+      Serial.println("CCS811: waiting for (new) data");
+    } else if( errstat & CCS811_ERRSTAT_I2CFAIL ) { 
+      Serial.println("CCS811: I2C error");
+    } else {
+      Serial.print("CCS811: errstat="); Serial.print(errstat,HEX); 
+      Serial.print("="); Serial.println( myCCS811.errstat_str(errstat) ); 
     }
   }
 }
